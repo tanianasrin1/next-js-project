ad9e2b843d8c8f6e2cae2d7fe063c6a931bbbd2e
tanianasrin1 <tasrineva169@gmail.com>
2022-10-26 11:09:43 +0600

    route added

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a>
+          {/* Welcome to <a href="https://nextjs.org">Next.js!</a> */}
+          hello world
         </h1>
 
         <p className={styles.description}>
